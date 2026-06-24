f689628a8ddc85d9671e3241607c2ee86fadb170
xavierflorensa <xavier.florensa@yahoo.com>
2026-06-24 19:39:58 +0200

    First commit: Alternative proposed by ChatGPT

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,61 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.UI;
+using FTOptix.DataLogger;
+using FTOptix.HMIProject;
+using FTOptix.NativeUI;
+using FTOptix.Store;
+using FTOptix.InfluxDBStoreRemote;
+using FTOptix.RAEtherNetIP;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.InfluxDBStore;
+using FTOptix.CommunicationDriver;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using InfluxDB3.Client;
+using InfluxDB3.Client.Write;
+
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+        
+        try
+        {
+            // InfluxDB v3 configuration
+            string url = "http://127.0.0.1:8181";
+            string token = "apiv3_33VhRYYjZdWdZQdum74hpF3tpX0pDNwmqpY7yOL_TCPTRM-KnEwbpYLr612L-T-9PDSZ0RJZsB38-LU5LYMFBQ";
+            string database = "OPTIX1";
+            string table = "DataLogger1";
+            
+            // Create InfluxDB v3 client
+            using (var client = new InfluxDBClient(host: url, token: token, database: database))
+            {
+                // Create write point for InfluxDB v3
+                var point = PointData.Measurement(table)
+                    .SetTag("host", "server01")
+                    .SetField("value", 5555)
+                    .SetTimestamp(DateTime.UtcNow);
+                
+                // Write data to InfluxDB v3 database
+                client.WritePointAsync(point: point).Wait();
+                Log.Info("Value 5555 inserted successfully into InfluxDB v3 database OPTIX1");
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.Error($"Error inserting value into InfluxDB: {ex.Message}");
+        }
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
