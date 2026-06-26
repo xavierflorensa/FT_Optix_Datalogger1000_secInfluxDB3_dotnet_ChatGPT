2e58a54abf98945ad8e52ada651c65414440a94a
xavierflorensa <xavier.florensa@yahoo.com>
2026-06-26 10:10:38 +0200

    Commit 5. Using a Netlogic called VariablePLC to make periodic task even lighter

--- ProjectFiles/NetSolution/RuntimeNetLogic1.cs
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -28,11 +28,14 @@ public class RuntimeNetLogic1 : BaseNetLogic
     private string influxDatabase;
     private string influxTable;
     private InfluxDBClient influxClient;
-
+    private IUAVariable variable1;
+    
     
     public override void Start()
     {
         // Insert code to be executed when the user-defined logic is started
+        //var variable1 = Project.Current.GetVariable("Model/Variable1");
+        variable1 = LogicObject.GetVariable("VariablePLC");
         // InfluxDB v3 configuration (initialize once)
         influxUrl = "http://127.0.0.1:8181";
         influxToken = "apiv3_33VhRYYjZdWdZQdum74hpF3tpX0pDNwmqpY7yOL_TCPTRM-KnEwbpYLr612L-T-9PDSZ0RJZsB38-LU5LYMFBQ";
@@ -41,7 +44,7 @@ public class RuntimeNetLogic1 : BaseNetLogic
         // Create InfluxDB client once and reuse it for every write
         influxClient = new InfluxDBClient(host: influxUrl, token: influxToken, database: influxDatabase);
 
-        myTask1 = new PeriodicTask(write_Function, 1000, LogicObject);
+        myTask1 = new PeriodicTask(write_Function, 100, LogicObject);
         myTask1.Start();
     }
 
@@ -57,7 +60,8 @@ public class RuntimeNetLogic1 : BaseNetLogic
         try
         {
             // Read the real-time value from the process variable
-            var variable1 = Project.Current.GetVariable("Model/Variable1");
+            //var variable1 = Project.Current.GetVariable("Model/Variable1");
+            
             int sencer = variable1.Value;
 
             // Reuse the InfluxDB client without recreating it each cycle
